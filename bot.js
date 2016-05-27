230b295a1e2caf08567d98ae95a586d013f3e9d0
alextanzilli <alextanzilli@gmail.com>
2016-05-26 23:32:01 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ var botID = process.env.BOT_ID;
 
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
-      botRegexliga = /^\/liga/;  botRegexGoat = /^\/goat/; botRegexGolf = /^\/golfthingorwhatever/;
+      botRegexliga = /^\/liga/;  botRegexGoat = /^\/goat/; botRegexGolf = /^\/golfthingorwhatever/; botRegexTony = /^\/tony/;
       botRegexSiege = /^\/siege/
       siege1 = 'https://i.groupme.com/350x419.png.adc8c73a6c1547e0a9e04320296329f8'; siege2 = 'https://i.groupme.com/1279x752.jpeg.aa5d0401e0df495bba4b4e09dc5a6bd7'
       siege3 = 'https://i.groupme.com/960x960.png.006e180e05d841c6a2962e844bf1e6fd';
@@ -27,6 +27,12 @@ function respond() {
     postMessage("http://ussoccerplayers.com/images/2013/02/alexi-lalas-us-soccer.jpg");
     this.res.end();
   } 
+    if(request.text && botRegexTony.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage("http://goldenagecheese.com/blog/wp-content/uploads/2015/05/Turkey-Pepperoni1.jpg");
+    this.res.end();
+  } 
+  
 
   else if(request.text && botRegexSiege.test(request.text)) {
     this.res.writeHead(200);
