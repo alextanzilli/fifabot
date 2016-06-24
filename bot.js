09f3817f1e7fcc3df6c13a0885ba938cbca9dd61
alextanzilli <alextanzilli@gmail.com>
2016-06-24 17:17:59 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,8 @@ var botID = process.env.BOT_ID;
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
       botRegexliga = /^\/liga/;  botRegexGoat = /^\/goat/; botRegexGolf = /^\/golfthingorwhatevr/; botRegexTony = /^\/tony/; botRegexTw = /^\/twitch/i;
-      botRegexSiege = /^\/siege/
+      botRegexToby = /^\/toby/;
+      botRegexSiege = /^\/siege/; 
       siege1 = 'https://i.groupme.com/350x419.png.adc8c73a6c1547e0a9e04320296329f8'; siege2 = 'https://i.groupme.com/1279x752.jpeg.aa5d0401e0df495bba4b4e09dc5a6bd7'
       siege3 = 'https://i.groupme.com/960x960.png.006e180e05d841c6a2962e844bf1e6fd';
   var teamAb = ["NE","NO","ARI","PHI","CLE","TEN","OAK","DAL","IND","SEA","CIN","PIT","JAC"
@@ -38,7 +39,11 @@ function respond() {
     postMessage("http://www.twitch.tv/"+request.text.substring(8,request.text.length));
     this.res.end();
   } 
-
+  else if(request.text && botRegexToby.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage("Get fucked, nerd.");
+    this.res.end();
+  } 
   else if(request.text && botRegexSiege.test(request.text)) {
     this.res.writeHead(200);
     if(0.6 >= Math.random() > 0.3)
